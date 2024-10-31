1cbcf076e8f87318029c5ec6cb8414cade208302
everett-p <sepatterso42@tntech.edu>
2024-10-31 13:40:24 -0500

    Start work on Menu Print

--- Driver.cpp
+++ Driver.cpp
@@ -1,28 +1,58 @@
 #include "LinkedList.h"
+#include "invitem.h"
 #include <iostream>
 
 using namespace std;
 
-void printMenu();
+int printMenu();
 void addItem();
 void editItem();
 void deleteItem();
 void iterate();
+void print();
 
 int main()
 {
+    List<InvItem> list;
+
 
 
     return 0;
 }
 
-void printMenu()
+int printMenu()
 {
-    
+    int a = 0;
+    cout << "*** WHAT WOULD YOU LIKE TO DO? ***\n";
+    cout << "\t1) PRINT Full INVENTORY \n\t2) ADD NEW ITEM \n\t3) EDIT ITEM \n\t4) DELETE ITEM \n\t5) EXIT SYSTEM\n";
+    cin >> a;
+    return a;  
 };
 
 void addItem()
 {
+    int b = 0;
+    cout << "Where would you like to add an Item?\n";
+    cout << "\t1) Add at the front \n\t2) Add at the end \n\t3) Add in a particlular place \n\t4) Go back";
+    cin >> b;
+
+    switch (b)
+    {
+        case 1:
+            
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+        case 4:
+            return;
+            break;
+        
+
+    }
 
 };
 
