5a69845f1efe4665d61dcb2919fbcd8aea5fdeae
RJMorriss <robertjmorriss@gmail.com>
2024-10-31 13:03:34 -0500

    export function definitions for LinkedList class

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -7,23 +7,235 @@
 
 #include "LinkedList.h"
 
-
-// PRIVATE
+// CONSTRUCTOR
 
 template <typename L>
+LinkedList<L>::LinkedList() 
+{
+    this->HEAD = NULL;
+    this->TAIL = NULL;
+}
+
+// DESTRUCTOR
+
+template <typename L>
+LinkedList<L>::~LinkedList() { delete HEAD; }
+
+// ADDING
+
+template <typename L> // append function
 void LinkedList<L>::append(L val)
 {
     ListNode<L>* newNode = new ListNode<L>(val);
-    if (!head) 
+    if (HEAD == NULL) 
     {
-        head = newNode; // If the list is empty, make newNode the head
+        HEAD = newNode; // If the list is empty, make newNode the head
         return;
     }
-    ListNode<L>* temp = head;
+    ListNode<L>* temp = HEAD;
     while (temp->next) 
     {
         temp = temp->next;
     }
     temp->next = newNode;
     return;
+}
+
+template <typename L> //prepend function
+void LinkedList<L>::prepend(L val)
+{
+    ListNode<L>* newNode = new ListNode<L>(val);
+    if (!HEAD) 
+    {
+        HEAD = newNode; // If the list is empty, make newNode the head
+    }
+    ListNode<L>* temp = HEAD;
+    newNode->next = HEAD;
+    HEAD = newNode
+    return;
+}
+
+template <typename L> //insert function
+void LinkedList<L>::insert(L val, int index)
+{
+    ListNode<L>* newNode = new ListNode<L>(val);
+    if (!HEAD) 
+    {
+        HEAD = newNode; // If the list is empty, make newNode the head
+        return;
+    }
+    ListNode<L>* temp = HEAD;
+    int test = 0;
+
+    // Traverse to the node just before the desired index
+    while (temp != nullptr && test < index - 1) 
+    {
+        temp = temp->next;      // Move to the next node
+        test++;         // Increment the current index
+    }
+    if (temp == nullptr)
+    {
+        cout << "OUT OF BOUNDS \n INSERTING AT END INSTEAD" << endl;
+    }
+    newNode->next = temp->next;
+    temp->next = newNode;
+    return;
+}
+
+// SWAPPING
+
+template <typename L>
+void LinkedList<L>::swap(L& a, L& b) 
+{
+    L temp = a; 
+    a = b;      
+    b = temp;  
+}
+
+// REMOVING
+
+template <typename L> //remove particular item
+void LinkedList<L>::deleteItem(int index)
+{
+    int test = 0;
+    ListNode<L>* temp = HEAD;
+    ListNode<L>* temp2 = HEAD;
+    while (temp != nullptr && test <= index) 
+    {
+        temp2 = temp;
+        temp = temp->next;
+        test++;         
+    }
+    if (temp == nullptr)
+    {
+        cout << "OUT OF BOUNDS \n FAILED TO DELETE ITEM" << endl;
+        return;
+    }
+    temp2->next = temp->next;
+    delete temp;
+}
+
+// ACCESSING
+
+template <typename L> //get first item
+L LinkedList<L>::getHead()
+{
+    if (HEAD == NULL) 
+    {
+        cout << "List is empty." << endl;
+        return;
+    }
+    return HEAD->value;
+}
+
+template <typename L> //get last item
+L LinkedList<L>::getTail()
+{
+    if (HEAD == NULL) 
+    {
+        cout << "List is empty." << endl;
+        return;
+    }
+    ListNode<L>* temp = HEAD;
+    ListNode<L>* temp2 = HEAD;
+    temp2 = temp2->next;
+
+    while (temp2 != nullptr)
+    {
+        temp = temp->next;
+        temp2 = temp2->next;
+    }
+
+    return temp->value;
+
+}
+
+template <typename L> //get particular item
+L LinkedList<L>::getItem(int index)
+{
+    if (HEAD == nullptr) 
+    {
+        cout << "List is empty." << endl;
+        return;
+    }
+
+    int test = 0;
+    ListNode<L>* temp = HEAD;
+    while (temp != nullptr && test <= index-1) 
+    {
+        temp = temp->next;
+        test++;         
+    }
+    if (temp == nullptr)
+    {
+        cout << "OUT OF BOUNDS \n GETTING END INSTEAD" << endl;
+    }
+
+    return temp->value;
+}
+
+// SORTING
+
+/*
+QuickSort
+*/
+
+template <typename L> //partition
+ListNode<L>* LinkedList<L>::partition(ListNode<L>* low, ListNode<L>* high, bool upDown)
+{
+    L pivot = high->getData();
+    ListNode<L>* i = low;
+
+    for (ListNode<L>* j = low ; j <= high ; j->iterate())
+    {
+        if ((upDown && j->getData() <= pivot) || (!upDown && j->getData() >= pivot)) 
+        {
+            if (i != j) 
+            {
+                swap(i->getData(), j->getData());
+            }
+        i = i->iterate();
+        }
+    }
+    swap(i->getData(), high->getData());
+    return i;
+}
+
+template <typename L> //quicksortRUN
+void LinkedList<L>::quicksortRUN(ListNode<L>* low, ListNode<L>* high, bool upDown)
+{
+    if (high != nullptr && low != high && low != high->iterate()) 
+    {
+        ListNode<L>* X = partition(low, high, upDown); 
+        quicksortRUN(low, X->iterate(), upDown); 
+        quicksortRUN(X->iterate(), high, upDown); 
+    }
+}
+
+template <typename L> //quicksort
+void LinkedList<L>::quickSort(bool upDown)
+{
+    ListNode<L>* high = HEAD;
+    while (high && high->iterate() != nullptr) 
+    {
+        high = high->iterate();
+    }
+    quicksortRUN(HEAD, high, upDown);
+}
+
+// OPERATOR OVERLOADING
+
+template <typename L>
+ostream& operator<<(ostream& stream, const LinkedList<L>& list) 
+{
+    ListNode<L>* temp = list.HEAD; // Start from the head
+    while (temp != nullptr)
+    {
+        int i = 1;
+        cout << i <<":"<< endl;
+        stream << temp->getData() << " "; // Output the data of each node
+        temp = temp->iterate(); // Move to the next node
+        i++;
+    }
+    return stream; // Return the stream
 }
--- linkedlist.h
+++ linkedlist.h
@@ -1,3 +1,10 @@
+/*
+*   Author: Everett Patterson, Robert J. Morriss
+*   Date: 10/28/24
+*   File: LinkedList.h
+*   Purpose: Header for LinkedList Class for CSC 1310 105 Program 2
+*/
+
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
 
@@ -11,222 +18,51 @@ class LinkedList
 {
     private:
     
-        ListNode<L>* head;
-
-        //append function
-        void append(L val);
-        // {
-        //     ListNode<L>* newNode = new ListNode<L>(val);
-        //     if (!head) 
-        //     {
-        //         head = newNode; // If the list is empty, make newNode the head
-        //         return;
-        //     }
-        //     ListNode<L>* temp = head;
-        //     while (temp->next) 
-        //     {
-        //         temp = temp->next;
-        //     }
-        //     temp->next = newNode;
-        //     return;
-        // }
-
-        //prepend function
-        void prepend(L val)
-        {
-            ListNode<L>* newNode = new ListNode<L>(val);
-            if (!head) 
-            {
-                head = newNode; // If the list is empty, make newNode the head
-            }
-            ListNode<L>* temp = head;
-            newNode->next = head;
-            head = newNode
-            return;
-        }
-
-        //insert function
-        void insert(L val, int index)
-        {
-            ListNode<L>* newNode = new ListNode<L>(val);
-            if (!head) 
-            {
-                head = newNode; // If the list is empty, make newNode the head
-                return;
-            }
-            ListNode<L>* temp = head;
-            int test = 0;
-
-            // Traverse to the node just before the desired index
-            while (temp != nullptr && test < index - 1) 
-            {
-                temp = temp->next;      // Move to the next node
-                test++;         // Increment the current index
-            }
-            if (temp == nullptr)
-            {
-                cout << "OUT OF BOUNDS \n INSERTING AT END INSTEAD" << endl;
-            }
-            newNode->next = temp->next;
-            temp->next = newNode;
-            return;
-        }
-
-        //get first item
-        L getHead()
-        {
-            if (head == nullptr) 
-            {
-                cout << "List is empty." << endl;
-                return;
-            }
-            return head->value;
-        }
-
-        //get last item
-        L getBack()
-        {
-            if (head == nullptr) 
-            {
-                cout << "List is empty." << endl;
-                return;
-            }
-            ListNode<L>* temp = head;
-            ListNode<L>* temp2 = head;
-            temp2 = temp2->next;
-
-            while (temp2 != nullptr)
-            {
-                temp = temp->next;
-                temp2 = temp2->next;
-            }
-
-            return temp->value;
-
-        }
-
-        //get particular item
-        L getItem(int index)
-        {
-            if (head == nullptr) 
-            {
-                cout << "List is empty." << endl;
-                return;
-            }
-
-            int test = 0;
-            ListNode<L>* temp = head;
-            while (temp != nullptr && test <= index-1) 
-            {
-                temp = temp->next;
-                test++;         
-            }
-            if (temp == nullptr)
-            {
-                cout << "OUT OF BOUNDS \n GETTING END INSTEAD" << endl;
-            }
-
-            return temp->value;
-        }
-
-        //remove particular item
-        void deleteItem(int index)
-        {
-            int test = 0;
-            ListNode<L>* temp = head;
-            ListNode<L>* temp2 = head;
-            while (temp != nullptr && test <= index) 
-            {
-                temp2 = temp;
-                temp = temp->next;
-                test++;         
-            }
-            if (temp == nullptr)
-            {
-                cout << "OUT OF BOUNDS \n FAILED TO DELETE ITEM" << endl;
-                return;
-            }
-            temp2->next = temp->next;
-            delete temp;
-        }
-
-
-        void swap(L& a, L& b) 
-        {
-            L temp = a; 
-            a = b;      
-            b = temp;  
-        }
-
+        ListNode<L>* HEAD;
+        ListNode<L>* TAIL;
 
     public:
 
-        List<L>() : head(nullptr) {} // Constructor
+        // CONSTRUCTOR
+
+        LinkedList();
+
+        // DESTRUCTOR
+
+        ~LinkedList();
+
+        // ADDING
+
+        void append(L);
+        void prepend(L);
+        void insert(L, int);
+
+        // SWAPPING
+
+        void swap(L&, L&);
+
+        // REMOVING
+
+        void deleteItem(int);
+
+        // ACCESSING
+
+        L getHead();
+        L getTail();
+        L getItem(int);
+
+        // SORTING
 
         /*
         QuickSort
         */
+        ListNode<L>* partition(ListNode<L>*, ListNode<L>*, bool);
+        void quicksortRUN(ListNode<L>*, ListNode<L>*, bool);
+        void quickSort(bool upDown);
 
-        //partition
-        ListNode<L>* partition(ListNode<L>* low, ListNode<L>* high, bool upDown)
-        {
-            L pivot = high->getData();
-            ListNode<L>* i = low;
+        // OPERATOR OVERLOADING
 
-            for (ListNode<L>* j = low ; j <= high ; j->iterate())
-            {
-                if ((upDown && j->getData() <= pivot) || (!upDown && j->getData() >= pivot)) 
-                {
-                    if (i != j) 
-                    {
-                        swap(i->getData(), j->getData());
-                    }
-                i = i->iterate();
-                }
-            }
-            swap(i->getData(), high->getData());
-            return i;
-        }
-
-        //quicksortRUN
-        void quicksortRUN(ListNode<L>* low, ListNode<L>* high, bool upDown)
-        {
-            if (high != nullptr && low != high && low != high->iterate()) 
-            {
-                ListNode<L>* X = partition(low, high, upDown); 
-                quicksortRUN(low, X->iterate(), upDown); 
-                quicksortRUN(X->iterate(), high, upDown); 
-            }
-        }
-
-        //quicksort
-        void quickSort(bool upDown)
-        {
-            ListNode<L>* high = head;
-            while (high && high->iterate() != nullptr) 
-            {
-                high = high->iterate();
-            }
-            quicksortRUN(head, high, upDown);
-        }
-
-
-        /*
-        
-        */
-    friend ostream& operator<<(ostream& stream, const List<L>& list) 
-    {
-        ListNode<L>* temp = list.head; // Start from the head
-        while (temp != nullptr)
-        {
-            int i = 1;
-            cout << i <<":"<< endl;
-            stream << temp->getData() << " "; // Output the data of each node
-            temp = temp->iterate(); // Move to the next node
-            i++;
-        }
-        return stream; // Return the stream
-    }
+        friend ostream& operator<<(ostream& stream, const List<L>& list);
 
 
 
